d04fcc02e4485a002572b6ab847261b642775487
JASON-LI-LAPTOP\jsnli <lijason@sheridancollege.ca>
2025-10-24 11:47:31 -0400

    Add responsive command dialog

--- src/app/(dashboard)/ui/components/dashboard-command.tsx
+++ src/app/(dashboard)/ui/components/dashboard-command.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
-import { CommandDialog, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { CommandResponsiveDialog, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 
 interface Props {
     open: boolean;
@@ -8,13 +8,13 @@ interface Props {
 
 export const DashboardCommand = ({ open, setOpen }: Props) => {
     return (
-        <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
             <CommandInput placeholder="Find a meeting or agent" />
             <CommandList>
                 <CommandItem>
                     Test
                 </CommandItem>
             </CommandList>
-        </CommandDialog>
+        </CommandResponsiveDialog>
     );
 };
