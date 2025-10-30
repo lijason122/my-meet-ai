fc5681b0cf56aa2d6f2bb159de767ed3afbd4097
JASON-LI-LAPTOP\jsnli <lijason@sheridancollege.ca>
2025-10-30 17:51:55 -0400

    Add loading state & error state for Meetings

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const MeetingsView = () => {
     const trpc = useTRPC();
-    const { data } = useQuery(trpc.meetings.getMany.queryOptions({}));
+    const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
 
     return (
         <div>
@@ -13,3 +15,15 @@ export const MeetingsView = () => {
         </div>
     );
 };
+
+export const MeetingsViewLoading = () => {
+    return (
+        <LoadingState title="Loading Meetings" description="This may take a few seconds" />
+    )
+}
+
+export const MeetingsViewError = () => {
+    return (
+        <ErrorState title="Error Loading Meetings" description="Please try again later" />
+    )
+}
