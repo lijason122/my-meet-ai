d3322e52d8eac169285ca070171160cb9327c54f
JASON-LI-LAPTOP\jsnli <lijason@sheridancollege.ca>
2025-10-31 20:43:43 -0400

    Modify procedures to include "duration" and inner-join agent

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -1,10 +1,10 @@
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { db } from "@/db";
-import { meetings } from "@/db/schema";
+import { agents, meetings } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
-import { and, count, desc, eq, getTableColumns, ilike } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import z from "zod";
 
 export const meetingsRouter = createTRPCRouter({
@@ -39,8 +39,14 @@ export const meetingsRouter = createTRPCRouter({
     })).query(async ({ ctx, input }) => {
         const { search, page, pageSize } = input;
 
-        const data = await db.select({ ...getTableColumns(meetings) })
+        const data = await db
+            .select({
+                ...getTableColumns(meetings),
+                agent: agents,
+                duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
+            })
             .from(meetings)
+            .innerJoin(agents, eq(meetings.agentId, agents.id))
             .where(and(eq(meetings.userId, ctx.auth.user.id), search ? ilike(meetings.name, `%${search}%`) : undefined))
             .orderBy(desc(meetings.createdAt), desc(meetings.id))
             .limit(pageSize)
@@ -48,6 +54,7 @@ export const meetingsRouter = createTRPCRouter({
 
         const [total] = await db.select({ count: count() })
             .from(meetings)
+            .innerJoin(agents, eq(meetings.agentId, agents.id))
             .where(and(eq(meetings.userId, ctx.auth.user.id), search ? ilike(meetings.name, `%${search}%`) : undefined));
 
         const totalPages = Math.ceil(total.count / pageSize);
