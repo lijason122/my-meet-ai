6d9b73aadb771980ea8be49dbe9598a2f60e4aaf
JASON-LI-LAPTOP\jsnli <lijason@sheridancollege.ca>
2025-11-12 16:13:11 -0500

    Add "meetings,getTranscript" and "meetings.generateChatToken" procedures

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -1,14 +1,16 @@
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { db } from "@/db";
-import { agents, meetings } from "@/db/schema";
+import { agents, meetings, user } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
-import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike, inArray, sql } from "drizzle-orm";
 import z from "zod";
-import { MeetingStatus } from "../types";
+import JSONL from "jsonl-parse-stringify";
+import { MeetingStatus, StreamTranscriptItem } from "../types";
 import { streamVideo } from "@/lib/stream-video";
 import { generateAvatarUri } from "@/lib/avatar";
+import { streamChat } from "@/lib/stream-chat";
 
 export const meetingsRouter = createTRPCRouter({
     create: protectedProcedure.input(meetingsInsertSchema).mutation(async ({ input, ctx }) => {
@@ -189,6 +191,77 @@ export const meetingsRouter = createTRPCRouter({
             exp: expirationTime,
             validity_in_seconds: issuedAt,
         });
+
+        return token;
+    }),
+    getTranscript: protectedProcedure.input(z.object({ id: z.string()} )).query(async ({ input, ctx }) => {
+        const [existingMeeting] = await db.select().from(meetings).where(
+            and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+        );
+
+        if (!existingMeeting) {
+            throw new TRPCError({ code: "NOT_FOUND", message: "meeting not found" });
+        }
+
+        if (!existingMeeting.transcriptUrl) {
+            return [];
+        }
+
+        const transcript = await fetch(existingMeeting.transcriptUrl)
+            .then((res) => res.text())
+            .then((text) => JSONL.parse<StreamTranscriptItem>(text))
+            .catch(() => {
+                return [];
+            });
+
+        const speakerIds = [...new Set(transcript.map((item) => item.speaker_id))];
+
+        const userSpeakers = await db.select().from(user).where(inArray(user.id, speakerIds))
+            .then((users) => users.map((user) => ({
+                ...user,
+                image: user.image ?? generateAvatarUri({ seed: user.name, variant: "initials" }),
+            }))
+        );
+
+        const agentSpeakers = await db.select().from(agents).where(inArray(agents.id, speakerIds))
+            .then((agents) => agents.map((agent) => ({
+                ...agent,
+                image: generateAvatarUri({ seed: agent.name, variant: "botttsNeutral" }),
+            }))
+        );
+
+        const speakers = [...userSpeakers, ...agentSpeakers];
+
+        const transcriptWithSpeakers = transcript.map((item) => {
+            const speaker = speakers.find((speaker) => speaker.id === item.speaker_id);
+
+            if (!speaker) {
+                return {
+                    ...item,
+                    user: {
+                        name: "Unknown",
+                        image: generateAvatarUri({ seed: "Unknown", variant: "initials" }),
+                    },
+                }
+            }
+
+            return {
+                ...item,
+                user: {
+                    name: speaker.name,
+                    image: speaker.image,
+                },
+            };
+        });
+
+        return transcriptWithSpeakers;
+    }),
+    generateChatToken: protectedProcedure.mutation(async ({ ctx }) => {
+        const token = streamChat.createToken(ctx.auth.user.id);
+        await streamChat.upsertUsers([{
+                id: ctx.auth.user.id,
+                role: "admin",
+        }]);
 
         return token;
     }),
