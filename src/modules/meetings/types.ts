74fc167a1d2206f0ab81cdf383fed4d1ed19e2e8
JASON-LI-LAPTOP\jsnli <lijason@sheridancollege.ca>
2025-11-21 15:22:24 -0500

    Fix Type error: Property 'items' does not exist on type

--- src/modules/meetings/types.ts
+++ src/modules/meetings/types.ts
@@ -1,8 +1,8 @@
-import { inferRouterInputs } from "@trpc/server";
+import { inferRouterOutputs } from "@trpc/server";
 import type { AppRouter } from "@/trpc/routers/_app";
 
-export type MeetingGetMany = inferRouterInputs<AppRouter>["meetings"]["getMany"]["items"];
-export type MeetingGetOne = inferRouterInputs<AppRouter>["meetings"]["getOne"];
+export type MeetingGetMany = inferRouterOutputs<AppRouter>["meetings"]["getMany"]["items"];
+export type MeetingGetOne = inferRouterOutputs<AppRouter>["meetings"]["getOne"];
 export enum MeetingStatus {
     Upcoming = "upcoming",
     Active = "active",
