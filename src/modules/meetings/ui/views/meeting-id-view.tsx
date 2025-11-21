3f2931a2cc38354eb7e68134d936ca79c67d6a8b
JASON-LI-LAPTOP\jsnli <lijason@sheridancollege.ca>
2025-11-21 15:08:04 -0500

    Remove "Cancel Meeting" button since it's redundant and user can delete the meeting

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -74,7 +74,7 @@ export const MeetingIdView = ({ meetingId }: Props) => {
                 {isProcessing && <ProcessingState />}
                 {isCompleted && <CompletedState data={data} />}
                 {isActive && <ActiveState meetingId={meetingId} />}
-                {isUpcoming && <UpcomingState meetingId={meetingId} onCancelMeeting={() => {}} isCancelling={false} />}
+                {isUpcoming && <UpcomingState meetingId={meetingId} />}
             </div>
         </>
     )
